ee70f9b653cc532baba292abb4778d2350b75758
Bruno Mendes <mendessbruno@gmail.com>
2021-11-03 18:19:59 -0300

    feat: updated controller for get all

--- back-end/controllers/taskController.js
+++ back-end/controllers/taskController.js
@@ -1,14 +1,16 @@
+const taskService = require('../services/taskService');
+
 // const HTTP_UNPROCESSABLE_ENTITY = 422;
 const HTTP_OK_STATUS = 200;
 // const HTTP_CREATED_STATUS = 201;
 
 const getTasks = (_req, res) => {
-  res.status(HTTP_OK_STATUS).json('olar');
+  const taskList = taskService.getAll();
+  res.status(HTTP_OK_STATUS).json(taskList);
 }
 
 const addTask = (_req, res) => {
   res.status(HTTP_OK_STATUS).json('olar');
-  console.log("isso ae");
 }
 
 module.exports = {
