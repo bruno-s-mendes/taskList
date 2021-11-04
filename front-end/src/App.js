8076d67d655aa8a381ba221594684e492b9ef47d
Bruno Mendes <mendessbruno@gmail.com>
2021-11-04 15:28:20 -0300

    refactoring: componet app from functiona to class component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
+import React, { Component } from 'react';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="">
         <h1>Lista de Tarefas</h1>
       </header>
       <AddTask/>
       <TaskList/>
     </div>
-  );
+    );
+  }
 }
-
-export default App;
