2ff675cdb6a86252086a71542e619ee1943b549f
Bruno Mendes <mendessbruno@gmail.com>
2021-11-04 18:13:45 -0300

    feat: render all tasks fetched

--- front-end/src/components/TaskList.js
+++ front-end/src/components/TaskList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Task from './Task';
 // import PropTypes from 'prop-types';
 
 export default class TaskList extends Component {
@@ -12,7 +13,7 @@ export default class TaskList extends Component {
   }
 
   render() {
-    console.log(this.props.taskList)
+    const list = this.props.taskList;
     return (
       <table>
         <tr>
@@ -21,6 +22,16 @@ export default class TaskList extends Component {
           <th>Prazo</th>
           <th>Status</th>
         </tr>
+        {list.length === 0 ? <span>Adicione uma Tarefa</span>
+          : list.map((item) => (
+            < Task 
+                id={item._id}
+                description={item.description}
+                creation={item.creation}
+                deadline={item.deadline}
+                status={item.status}
+            />
+          )) }       
         <tr>
           <td>Assistir Aula</td>
           <td>04/11/2021</td>
